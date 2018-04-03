7e21723cd971fa1c7cebca640408e4ee70f1adc8
Jacob <gg.baibo@gmail.com>
2018-04-03 10:47:42 +0800

    add two number v1 finish

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -19,65 +19,54 @@ class ListNode:
         self.next = None
 
 class Solution:
+
+    """
+	128ms
+	>64.36
+	https://leetcode.com/submissions/detail/148243044/
+    """
     def addTwoNumbers_v1(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
-        l1_length = 1
-        l2_length = 1
-        l1_new = l1
-        l2_new = l2
-        while l1_new.next or l2_new.next:
-            if l1_new.next:
-                l1_length += 1
-                l1_new = l1_new.next
-            if l2_new.next:
-                l2_length += 1
-                l2_new = l2_new.next
-
-        if l1_length - l2_length >= 0:
-            while l1_length - l2_length - 1 >= 0:
-                l2_length += 1
-                l = ListNode(0)
-                l.next = l2
-                l2 = l
-        else:
-            while l2_length - l1_length - 1 >= 0:
-                l1_length += 1
-                l = ListNode(0)
-                l.next = l1_new
-                l1 = l
-
         l3 = ListNode(0)
-        l4 = l3
-        l3.next = l4
         carry = 0
-        while l1:
-            l3_temp = ListNode(l1.val + l2.val + carry)
-            if l3_temp.val - 9 > 0:
+        l3_pointer = l3
+        l3.next = l3_pointer
+        while l1 or l2:
+            l1_val = l1.val if l1 else 0
+            l2_val = l2.val if l2 else 0
+            temp = ListNode(l1_val + l2_val + carry)
+            if temp.val > 9:
                 carry = 1
-                l3_temp.val = l3_temp.val - 10
+                temp.val = temp.val - 10
             else:
                 carry = 0
 
-            l1 = l1.next
-            l2 = l2.next
-            l4.next = l3_temp
-            l4 = l3_temp
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            l3_pointer.next  = temp
+            l3_pointer = l3_pointer.next
+
 
         if carry == 1:
-            l4.val = l4.val - 10
-            l4.next = ListNode(1)
+            l3_pointer.next = ListNode(1)
 
         return l3.next
 
+
+
+
 if "__main__" == __name__:
     solution = Solution()
-    l1 = ListNode(1)
-    l1.next = ListNode(8)
-    l2 = ListNode(0)
+    l1 = ListNode(2)
+    l1.next = ListNode(4)
+    l1.next.next = ListNode(3)
+    l2 = ListNode(5)
+    l2.next = ListNode(6)
+    l2.next.next = ListNode(4)
     l3 = solution.addTwoNumbers_v1(l1, l2)
     while l3:
         print(l3.val, " -> ")
